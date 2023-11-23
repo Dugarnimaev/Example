b6ee3c56d9a244896992a0e98a403a5ecd81e32c
admin <bul17at62@gmail.com>
2023-11-23 15:18:59 +0900

    19.11.23 Начало работы

---
+++ 59b.cpp
@@ -0,0 +1,24 @@
+﻿// 59 Даны действительные числа x, y. Определить, принадлежит ли точка с координатами x, y заштрихованной части плоскости
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    const float r1 = 0.5, R = 1;
+    float a, x, y;
+    cout << "Input X: " << endl;
+    cin >> x;
+    cout << "Input Y: " << endl;
+    cin >> y;
+    a = pow(x, 2) + pow(y, 2);
+    if (r1 < a && a < R) {
+        cout << "Yes ";
+    }
+     else {
+        cout << "No";
+    }
+    return 0;
+}
